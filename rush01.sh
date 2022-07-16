a3fdad452be3d97452c8c1cfdee50cd6157e93da
Henrique Mochida <hmochida@student.42sp.org.br>
2022-07-16 18:05:38 -0300

    feat(rush01): adicionado o script do flavio

---
+++ rush01.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]] ; then
+	echo -e '\nUsage:\n\t./gen.sh board_size\n';
+	exit 0;
+fi
+
+python3 ./gen.py $1 > tmp;
+cat tmp;
+echo ;
+cat tmp | head -1 | sed 's/ /\//g' | sed 's/\/$//' | xargs echo "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/js/towers.html?#$1:" | sed 's/ //'
+echo ;
+rm tmp;
